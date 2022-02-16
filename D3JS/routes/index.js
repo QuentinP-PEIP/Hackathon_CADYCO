3e68e7d65b088c9747f1d9b59959037f8a661bb8
Souchon <72502457+romainsouchon@users.noreply.github.com>
2022-02-16 14:17:35 +0100

    Suppression D3JS et ajout excel modifie power BI

--- D3JS/routes/index.js
+++
@@ -1,9 +0,0 @@
-let express = require('express');
-let router = express.Router();
-
-/* GET home page. */
-router.get('/mass_shooting_us', function(req, res, next) {
-  res.render('index', {});
-});
-
-module.exports = router;
